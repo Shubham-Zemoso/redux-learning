2a1553948e07eb25dc8de85c16e1e8ffb7d31dde
shubham-zemoso <shubham.kukreti@zemosolabs.com>
2022-08-06 20:44:44 +0530

    create-async-thunk

--- Redux-toolkit/index.js
+++ Redux-toolkit/index.js
@@ -1,22 +1,26 @@
 const store = require("./app/store");
 const cakeActions = require("./features/cake/cakeSlice").cakeActions;
 const icecreamActions = require("./features/icecream/iceCreamSlice").icecreamActions;
+const fetchUsers = require('./features/user/userSlice').fetchUsers
+
 console.log("Initial state", store.getState());
 
 const unsubscribe = store.subscribe(() => {
   console.log("Updated state ", store.getState());
 });
 
-store.dispatch(cakeActions.ordered());
-store.dispatch(cakeActions.ordered());
-store.dispatch(cakeActions.ordered());
-store.dispatch(cakeActions.restocked(3));
+store.dispatch(fetchUsers())
 
-store.dispatch(icecreamActions.ordered());
-store.dispatch(icecreamActions.ordered());
-store.dispatch(icecreamActions.ordered());
-store.dispatch(icecreamActions.restocked(3));
+// store.dispatch(cakeActions.ordered());
+// store.dispatch(cakeActions.ordered());
+// store.dispatch(cakeActions.ordered());
+// store.dispatch(cakeActions.restocked(3));
+
+// store.dispatch(icecreamActions.ordered());
+// store.dispatch(icecreamActions.ordered());
+// store.dispatch(icecreamActions.ordered());
+// store.dispatch(icecreamActions.restocked(3));
 
 
 
-unsubscribe();
+// unsubscribe();
